6cce43a1663439d313d6b4a2726a8c1de6a81039
ARAYE Anthony <anthonynjiva@free.fr>
2018-03-01 12:25:17 +0100

    Ajout des fichiers de tests pour l'arrêt/réveil de processus

---
+++ test/sleep_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int statut = 0;
+    statut = syscall(335);
+    if (statut == -1)
+    {
+        fprintf(stderr, "%s : sc335 : %s\n", argv[0], strerror(errno));
+        exit(1);
+    }
+    printf("Bien reveillé\n");
+    return 0;
+}
---
+++ test/wakeup_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int statut = 0;
+    statut = syscall(336);
+    if (statut == -1)
+    {
+        fprintf(stderr, "%s : sc336 : %s\n", argv[0], strerror(errno));
+        exit(1);
+    }
+    printf("Envoi d'un signal de réveil\n");
+    return 0;
+}
