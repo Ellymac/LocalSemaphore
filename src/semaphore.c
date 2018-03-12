3de9189e7822f55223e7a5caa46a72e2e264144c
Ellymac <cschnell67@gmail.com>
2018-03-12 15:45:12 +0100

    up semaphore.c with vmalloc and kfree functions

--- src/semaphore.c
+++ src/semaphore.c
@@ -1,15 +1,32 @@
 #include <linux/kernel.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <linux/mm.h>
+#include <linux/tty.h>
+#include <linux/tty_driver.h>
+#include <linux/console.h>
+#include <linux/init.h>
+#include <linux/jiffies.h>
+#include <linux/nmi.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/interrupt.h>			/* For in_interrupt() */
+#include <linux/delay.h>
+#include <linux/smp.h>
+#include <linux/security.h>
+#include <linux/bootmem.h>
+#include <linux/syscalls.h>
+#include <linux/kexec.h>
+
+#include <asm/uaccess.h>
+
 #include "semaphore.h"
 
 sem sem_initialize(int nb)
 {
-    sem t = malloc(sizeof(struct t_sem));
+    sem t = vmalloc(sizeof(struct t_sem));
     t->nb_max = nb;
     t->nb_available = nb;
     t->id = 0;
-    t->waitlist = malloc(sizeof(int));
+    t->waitlist = vmalloc(sizeof(int));
     return t;
 }
 
@@ -19,14 +36,14 @@ int sem_destroy(int sid)
     c = current;
     sem s = c->tab_sem[sid];
     int *tab = s->waitlist;
-    free(tab);
-    free(s);
+    kfree(tab);
+    kfree(s);
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
     sem result = sem_initialize(2);
-    printf("%d", result->nb_max);
+    printk(KERN_DEBUG "%d", result->nb_max);
     return 0;
 }
